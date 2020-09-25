53ea47485d574f6a65bd97a22085cdbb9baebfe9
Rachit Gupta <guptarachit2004@gmail.com>
2020-09-26 01:26:18 +0530

    frontend: patient home + prescription history screens

---
+++ lib/pages/patient-specific/homePatient.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:soochit/global/myColors.dart';
+import 'package:soochit/global/myDimens.dart';
+import 'package:soochit/global/mySpaces.dart';
+import 'package:soochit/global/myStrings.dart';
+import 'package:soochit/widgets/medicineDeadlineReminder.dart';
+
+class HomePatient extends StatefulWidget {
+  static String id = "homePatient";
+  @override
+  _HomePatientState createState() => _HomePatientState();
+}
+
+class _HomePatientState extends State<HomePatient> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(MyDimens.double_30),
+      child: Column(
+        children: [
+          MySpaces.vMediumGapInBetween,
+          Text(
+            MyStrings.patientMedDeadlines, style: Theme.of(context).textTheme.headline4.copyWith(color: MyColors.primaryColor, fontFamily: 'airbnb'),
+          ),
+          MySpaces.vLargeGapInBetween,
+          Column(
+            children: [
+                MedicineDeadlineReminder(),
+                MySpaces.vMediumGapInBetween,
+                MedicineDeadlineReminder(),
+                MySpaces.vMediumGapInBetween,
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/patient-specific/prescriptionHistory.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:soochit/global/myColors.dart';
+import 'package:soochit/global/myDimens.dart';
+import 'package:soochit/global/mySpaces.dart';
+import 'package:soochit/global/myStrings.dart';
+import 'package:soochit/widgets/prescriptionHistoryCard.dart';
+
+class PrescriptionHistory extends StatefulWidget {
+  static String id = "prescriptionHistory";
+  @override
+  _PrescriptionHistoryState createState() => _PrescriptionHistoryState();
+}
+
+class _PrescriptionHistoryState extends State<PrescriptionHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(MyDimens.double_30),
+      child: Column(
+        children: [
+          MySpaces.vMediumGapInBetween,
+          Text(
+            MyStrings.patientPrescriptionHistoryLabel, style: Theme.of(context).textTheme.headline4.copyWith(color: MyColors.primaryColor, fontFamily: 'airbnb'),
+          ),
+          MySpaces.vLargeGapInBetween,
+          Column(
+            children: [
+              PrescriptionHistoryCard(),
+              MySpaces.vSmallGapInBetween,
+              PrescriptionHistoryCard(),
+              MySpaces.vSmallGapInBetween,
+              PrescriptionHistoryCard(),
+              MySpaces.vSmallGapInBetween,
+              PrescriptionHistoryCard()
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
