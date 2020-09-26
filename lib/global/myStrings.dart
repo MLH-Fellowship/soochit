898043c0c1ce6a8936fb8aa9562434eec6b23cb3
Rachit Gupta <guptarachit2004@gmail.com>
2020-09-26 19:18:06 +0530

    authetication: firebase auth using otp (#10)
    * made a flutter run, got .apk files
    
    * added and configured firebase, added wokring OTP auth code (ui remainign)
    
    * added UI to OTPAuth pages, repo structure needs some more work still
    
    * refactored code, and made it cuter. Added widgets and constants
    
    * made a flutter clean and removed builds
    
    Co-authored-by: Imperial-lord <absatyaprakash01@gmail.com>
    Co-authored-by: AB Satyaprakash <45942031+Imperial-lord@users.noreply.github.com>

--- lib/global/myStrings.dart
+++ lib/global/myStrings.dart
@@ -8,6 +8,22 @@ class MyStrings {
   static const doctorLabel = "Doctor";
   static const patientLabel = "Patient";
 
+  // loginAuth screen
+  static const buttonLabel = "Send Security Code";
+  static const phoneRequest = "Enter your mobile number to get an OTP";
+  static const placeholderLoginAuth = 'Phone Number (include +91)';
+
+  // otpAuth screen
+  static const buttonLabel1 = "Confirm OTP";
+  static const otpRequest = "Enter the (6 digit) OTP you have received";
+
+  // snackBar messages (errors/suggestions)
+  static const invalidPhoneNumber = 'Please enter a valid phone number';
+  static const invalidCodeOrAuth = 'Invalid code/invalid authentication';
+  static const somethingGoneWrong = 'Something has gone wrong, please try later';
+  static const invalidPhoneNumberFormat = 'The phone number format is incorrect. Please enter your number in E.164 format. [+][country code][number]';
+  static const wrongCodeInput = 'Wrong code ! Please enter the last code received.';
+
   //dummy data
   static const patientName = "Rachit Gupta";
   static const paracetamolLabel = "Paracetamol";
@@ -24,9 +40,6 @@ class MyStrings {
   //prescription history
   static const patientPrescriptionHistoryLabel = "$patientName's Prescription History";
 
-  //TODO: >>>>>>>>>>>>>>>>>>>> DOCTOR >>>>>>>>>>>>>>>>>>>>
-
-
-
-
+//TODO: >>>>>>>>>>>>>>>>>>>> DOCTOR >>>>>>>>>>>>>>>>>>>>
 }
+
---
+++ lib/pages/authentication/login_page.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:soochit/global/myDimens.dart';
+import 'package:soochit/global/myStrings.dart';
+import 'package:soochit/global/myColors.dart';
+import 'package:soochit/stores/login_store.dart';
+import 'package:soochit/widgets/loader_hud.dart';
+import 'package:soochit/widgets/snackbar.dart';
+
+class LoginPage extends StatefulWidget {
+  static String id = "login_page";
+
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LoginStore>(
+      builder: (_, loginStore, __) {
+        return Observer(
+          builder: (_) => LoaderHUD(
+            inAsyncCall: loginStore.isLoginLoading,
+            child: Material(
+              child: Scaffold(
+                backgroundColor: MyColors.primaryColor,
+                key: loginStore.loginScaffoldKey,
+                body: SafeArea(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      child: Column(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                margin: EdgeInsets.only(
+                                  left: MyDimens.double_30,
+                                  right: MyDimens.double_30,
+                                  top: MyDimens.double_25 * 2,
+                                ),
+                                child: Column(
+                                  // ignore: prefer_const_literals_to_create_immutable
+                                  children: <Widget>[
+                                    // ignore: prefer_const_literals_to_create_immutables, prefer_const_literals_to_create_immutables
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(
+                                        MyStrings.welcomeToLabel,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline4
+                                            .copyWith(
+                                                color: MyColors.lighterPink,
+                                                fontFamily: 'airbnb',
+                                                fontSize: 30),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text(MyStrings.appName,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline3
+                                              .copyWith(
+                                                  color: MyColors.white,
+                                                  fontFamily: 'airbnb')),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: MyDimens.double_30,
+                                      vertical: MyDimens.double_30),
+                                  child: Image(
+                                    image: AssetImage(
+                                        'assets/images/medicine.png'),
+                                    fit: BoxFit.cover,
+                                  )),
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: MyDimens.double_30),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(MyStrings.phoneRequest,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText2
+                                          .copyWith(
+                                              color: MyColors.white,
+                                              fontFamily: 'lexenddeca')),
+                                ),
+                              ),
+                              Container(
+                                height: 3 * MyDimens.double_20,
+                                constraints: BoxConstraints(
+                                    maxWidth: MyDimens.double_600),
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: MyDimens.double_30,
+                                    vertical: MyDimens.double_20),
+                                child: CupertinoTextField(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: MyDimens.double_15),
+                                    decoration: BoxDecoration(
+                                        color: MyColors.lightPink,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(
+                                                MyDimens.double_4))),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6
+                                        .copyWith(
+                                            color: MyColors.lightestPink,
+                                            fontFamily: 'lexenddeca'),
+                                    controller: phoneController,
+                                    clearButtonMode:
+                                        OverlayVisibilityMode.editing,
+                                    keyboardType: TextInputType.phone,
+                                    maxLines: 1,
+                                    placeholder: MyStrings.placeholderLoginAuth,
+                                    placeholderStyle: Theme.of(context)
+                                        .textTheme
+                                        .subtitle1
+                                        .copyWith(
+                                            color: MyColors.lightestPink,
+                                            fontFamily: 'lexenddeca')),
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(
+                                    horizontal: MyDimens.double_30,
+                                    vertical: MyDimens.double_10),
+                                constraints: BoxConstraints(
+                                    maxWidth: MyDimens.double_600),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: OutlineButton(
+                                        onPressed: () {
+                                          if (phoneController.text.isNotEmpty) {
+                                            loginStore.getCodeWithPhoneNumber(
+                                                context,
+                                                phoneController.text
+                                                    .toString());
+                                          } else {
+                                            loginStore
+                                                .loginScaffoldKey.currentState
+                                                .showSnackBar(getSnackBar(
+                                                    context,
+                                                    MyStrings
+                                                        .invalidPhoneNumber));
+                                          }
+                                        },
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                                MyDimens.double_4)),
+                                        borderSide: BorderSide(
+                                            color: MyColors.lighterPink,
+                                            width: MyDimens.double_1),
+                                        color: MyColors.primaryColor,
+                                        child: Padding(
+                                          padding: EdgeInsets.only(
+                                              top: MyDimens.double_15,
+                                              bottom: MyDimens.double_15),
+                                          child: Text(MyStrings.buttonLabel,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .subtitle1
+                                                  .copyWith(
+                                                      color:
+                                                          MyColors.lighterPink,
+                                                      fontFamily:
+                                                          'lexenddeca')),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
